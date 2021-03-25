9800974bf42507820cca50c70cf2244f8ada035e
Melanie Daoheuang <daoh0031@gmail.com>
2021-03-24 19:30:52 -0600

    Complete the rest of the round unit tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -16,6 +16,31 @@ describe('Round', function() {
     const deck = new Deck();
     const round = new Round(deck);
 
-    expect(round.returnCurrentCard()).to.deep.equal();
+    expect(round.returnCurrentCard()).to.deep.equal(2, 'How many hearts do octopuses have?', [1, 2, 3], 3);
+  });
+
+  it.skip('should count each guess as a turn', function() {
+    const deck = new Deck();
+    const round = new Round(deck);
+
+    expect(round.turns).to.deep.equal(0);
+
+    round.takeTurn();
+
+    expect(round.turns).to.deep.equal(1);
+  }); 
+
+  it.skip('should record incorrect guesses', function() {
+    const deck = new Deck();
+    const round = new Round(deck);
+
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it.skip('should calculate the percentage of correct guesses', function() {
+    const deck = new Deck();
+    const round = new Round(deck);
+
+    expect(round.calculatePercentCorrect).to.deep.equal(100);
   });
 });
